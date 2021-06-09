7e945e9ef6d08f5633bd0a6d2d405569e7378d41
WeCryAlone <75624361+WeCryAlone@users.noreply.github.com>
2021-06-09 12:45:51 +0200

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Client : Person
+    {
+        public Message Info;
+        public Message Error;
+
+        public int Sum { get; set; } = 0;    // сумма на счету
+
+        int Money = 1500;
+
+        bool account = false;
+
+        public void OpenAccount()
+        {
+            if(account == false)
+            {
+                Info?.Invoke("Введите имя");
+                Name = Convert.ToString(Console.ReadLine());
+                Info?.Invoke("Введите Фамилию");
+                LastName = Convert.ToString(Console.ReadLine());
+                account = true;
+                Info?.Invoke("Счёт на имя: '" + Name + " " + LastName + "' Открыт");
+            }
+            else
+            {
+                Error?.Invoke("У вас и так открыт счёт");
+            }
+        }
+
+        public void SetMoney()
+        {
+            if(account == true)
+            {
+                if (Money == 0 || Money < 0)
+                {
+                    Error?.Invoke("У вас нет денег что бы положить на счёт");
+                }
+                else
+                {
+                    Info?.Invoke("У вас на счету " + Sum + " Денег");
+                    Info?.Invoke("У вас " + Money + " Наличных");
+                    Info?.Invoke("Сколько желаете положить?");
+                    int answerSetMoney = Convert.ToInt32(Console.ReadLine());
+                    if(answerSetMoney > Money)
+                    {
+                        Error?.Invoke("Вы не можете положить больше чем у вас есть");
+                    }
+                    else
+                    {
+                        Sum = Sum + answerSetMoney;
+                        Money = Money - answerSetMoney;
+                        Info?.Invoke("Вы положили: " + answerSetMoney);
+                        Info?.Invoke("У вас на счету теперь: " + Sum);
+                        Info?.Invoke("У вас денег теперь: " + Money);
+                    }
+                }
+                
+            }
+            else
+            {
+                Error?.Invoke("Вы не можете положить деньги поскольку у вас закрыт счёт");
+            }
+        }
+
+        public void TakeMoney()
+        {
+            if(account == true)
+            {
+                if(Sum == 0 || Sum < 0)
+                {
+                    Error?.Invoke("Вы не можете снять деньги по скольку их нет на счету");
+                }
+                else
+                {
+                    Info?.Invoke("Сколько желаете снять?");
+                    int answerTakeMoney = Convert.ToInt32(Console.ReadLine());
+                    if(answerTakeMoney > Sum)
+                    {
+                        Error?.Invoke("Вы не можете снять больше чем есть на счету");
+                    }
+                    else
+                    {
+                        Sum = Sum - answerTakeMoney;
+                        Money = Money + answerTakeMoney;
+                        Info?.Invoke("Вы Сняли: " + answerTakeMoney);
+                        Info?.Invoke("У вас на счету теперь: " + Sum);
+                        Info?.Invoke("У вас денег теперь: " + Money);
+                    }
+                }
+            }
+            else
+            {
+                Error?.Invoke("Вы не можете снять деньги поскольку у вас закрыт счёт");
+            }
+        }
+
+        public void CloseAccount()
+        {
+            if(account == true)
+            {
+                account = false;
+                Info?.Invoke("Вы закрыли счёт");
+            }
+            else
+            {
+                Error?.Invoke("Счёт и так закрыт");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Приветствуем вас в банке RoflBank, что вы хотите сделать?");
+            
+                Client client = new Client();
+                client.Info = ShowConsoleInfo;
+                client.Error = ShowConsoleError;
+            for (; ; )
+            {
+                Console.WriteLine("1. Открыть счёт");
+                Console.WriteLine("2. Внести деньги");
+                Console.WriteLine("3. Снять деньги");
+                Console.WriteLine("4. Закрыть счёт");
+                int answerStart = Convert.ToInt32(Console.ReadLine());
+                if (answerStart == 1) client.OpenAccount();
+                if (answerStart == 2) client.SetMoney();
+                if (answerStart == 3) client.TakeMoney();
+                if (answerStart == 4) client.CloseAccount(); 
+            }
+        }
+
+        static void ShowConsoleInfo(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+        static void ShowConsoleError(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+    }
+}
